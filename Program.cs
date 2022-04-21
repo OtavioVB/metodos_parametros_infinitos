f7dcb708de172751b08a8e9800f25c2d88b7c603
Otávio <101956836+OtavioVB@users.noreply.github.com>
2022-04-21 13:20:49 -0300

    UPDATE CLEAN CODE

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace InfinitosParametros
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Media(5, 3, 4, 5, 10, 123, 3).ToString());
-            Console.WriteLine(Soma(5, 3, 4, 5, 10, 123, 3).ToString());
+            Console.WriteLine(CalcularMedia(5, 3, 4, 5, 10, 123, 3).ToString());
+            Console.WriteLine(CalcularSoma(5, 3, 4, 5, 10, 123, 3).ToString());
             
             /* INTERESSANTE PARA ADICIONAR EM FORMULÁRIOS NAS QUAIS POSSUEM 
             MUITOS COMPONENTES QUE NECESSITAM ESTAR PREENCHIDOS*/
@@ -17,7 +17,7 @@ namespace InfinitosParametros
             Console.ReadLine();
         }
 
-        public static int Media(params int[] valores)
+        public static int CalcularMedia(params int[] valores)
         {
             int TotalSoma = 0;
             foreach (int i in valores)
@@ -27,7 +27,7 @@ namespace InfinitosParametros
             return TotalSoma/valores.Length;
         }
 
-        public static int Soma(params int[] valores)
+        public static int CalcularSoma(params int[] valores)
         {
             int Soma = 0;
             foreach (int i in valores)
